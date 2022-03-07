28a434acf47c6ecbd5294ef0b68c2dc5d45022d4
jongvnnar <jgh060@gmail.com>
2022-03-07 18:11:29 +0000

    feat: OrderState enum

---
+++ src/lib/order-state.js
@@ -0,0 +1,79 @@
+/**
+ * Class to handle possible state order can be in
+ */
+export class OrderState {
+  static new = new OrderState(0);
+  static prepare = new OrderState(1);
+  static cooking = new OrderState(2);
+  static ready = new OrderState(3);
+  static finished = new OrderState(4);
+
+  constructor(val) {
+    if (val < 5 && val >= 0) {
+      this.val = val;
+      return;
+    }
+    throw new Error('Attempt to initialize to unauthorized state');
+  }
+
+  /**
+   * Returns state name
+   */
+  get name() {
+    switch (this.val) {
+      case 0:
+        return 'NEW';
+      case 1:
+        return 'PREPARE';
+      case 2:
+        return 'COOKING';
+      case 3:
+        return 'READY';
+      case 4:
+        return 'FINISHED';
+      default:
+        throw new Error('Unauthorized state');
+    }
+  }
+
+  /**
+   * Return OrderState for each possible state
+   * @param {string} name "NEW" | "PREPARE" | "COOKING" | "READY" | "FINISHED"
+   * @returns OrderState instance for possible state
+   */
+  static fromString(name) {
+    switch (name) {
+      case 'NEW':
+        return OrderState.new;
+      case 'PREPARE':
+        return OrderState.prepare;
+      case 'COOKING':
+        return OrderState.cooking;
+      case 'READY':
+        return OrderState.ready;
+      case 'FINISHED':
+        return OrderState.finished;
+      default:
+        throw new Error('Unauthorized state');
+    }
+  }
+
+  /**
+   * Get next state from current one.
+   * @returns next possible state
+   */
+  getNextState() {
+    switch (this.val) {
+      case 0:
+        return OrderState.prepare;
+      case 1:
+        return OrderState.cooking;
+      case 2:
+        return OrderState.ready;
+      case 3:
+        return OrderState.finished;
+      default:
+        throw new Error('Attempt to reach unauthorized state');
+    }
+  }
+}
---
+++ src/test/orderstate.test.js
@@ -0,0 +1,83 @@
+import { describe, expect, test } from '@jest/globals';
+import { OrderState } from '../../src/lib/order-state.js';
+/**
+ * OrderState enum
+ */
+describe('OrderState enum', () => {
+  const NEW = 'NEW';
+  const PREPARE = 'PREPARE';
+  const COOKING = 'COOKING';
+  const READY = 'READY';
+  const FINISHED = 'FINISHED';
+  const UNAUTHORIZED_MESSAGE = 'Unauthorized state';
+  test('has NEW state', () => {
+    const result = OrderState.new.name;
+    expect(result).toBe(NEW);
+  });
+  test('has PREPARE state', () => {
+    const result = OrderState.prepare.name;
+    expect(result).toBe(PREPARE);
+  });
+  test('has COOKING state', () => {
+    const result = OrderState.cooking.name;
+    expect(result).toBe(COOKING);
+  });
+  test('has READY state', () => {
+    const result = OrderState.ready.name;
+    expect(result).toBe(READY);
+  });
+  test('has FINISHED state', () => {
+    const result = OrderState.finished.name;
+    expect(result).toBe(FINISHED);
+  });
+
+  test('NEW goes to PREPARE state', () => {
+    const result = OrderState.new.getNextState().name;
+    expect(result).toBe(PREPARE);
+  });
+  test('PREPARE goes to COOKING state', () => {
+    const result = OrderState.prepare.getNextState().name;
+    expect(result).toBe(COOKING);
+  });
+  test('COOKING goes to READY state', () => {
+    const result = OrderState.cooking.getNextState().name;
+    expect(result).toBe(READY);
+  });
+  test('READY goes to FINISHED state', () => {
+    const result = OrderState.ready.getNextState().name;
+    expect(result).toBe(FINISHED);
+  });
+  test('Calling getNextState on FINISHED throws error', () => {
+    expect(() => OrderState.finished.getNextState()).toThrow(
+      'Attempt to reach unauthorized state'
+    );
+  });
+  test('get state from string with NEW returns correctly', () => {
+    const result = OrderState.fromString('NEW');
+    expect(result.name).toBe(NEW);
+  });
+  test('get state from string with PREPARE returns correctly', () => {
+    const result = OrderState.fromString('PREPARE');
+    expect(result.name).toBe(PREPARE);
+  });
+  test('get state from string with COOKING returns correctly', () => {
+    const result = OrderState.fromString('COOKING');
+    expect(result.name).toBe(COOKING);
+  });
+  test('get state from string with READY returns correctly', () => {
+    const result = OrderState.fromString('READY');
+    expect(result.name).toBe(READY);
+  });
+  test('get state from string with FINISHED returns correctly', () => {
+    const result = OrderState.fromString('FINISHED');
+    expect(result.name).toBe(FINISHED);
+  });
+  test('get state from empty string throws', () => {
+    expect(() => OrderState.fromString('')).toThrow(UNAUTHORIZED_MESSAGE);
+  });
+  test('get state from bullshit string throws', () => {
+    expect(() => OrderState.fromString('aldkfslkdasjfl')).toThrow(
+      UNAUTHORIZED_MESSAGE
+    );
+  });
+});
