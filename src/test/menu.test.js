d45d33529d9b7d62321e78e633c5815175d69725
Ben-Ivars <60788806+Ben-Ivars@users.noreply.github.com>
2022-03-17 19:17:33 +0000

    menu test update

--- src/test/menu.test.js
+++ src/test/menu.test.js
@@ -1,23 +1,35 @@
 import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
-import { createSchema, dropSchema, end } from '../lib/db';
-import {
-  postAndParse
-} from './utils';
+import { createSchema, dropSchema, end, insertData } from '../lib/db';
+import { fetchAndParse, postAndParse } from './utils';
 
 describe('menus', () => {
   beforeAll(async () => {
     await dropSchema();
     await createSchema();
+    await insertData();
   });
 
   afterAll(async () => {
     await end();
   });
 
-  test('POST /menu creates a new menu item', async () => {
-    const { result, status } = await postAndParse('/menu', null);
+  test('GET /menu gets all menu items', async () => {
+    const { result, status } = await fetchAndParse('/menu', null);
 
-    expect(status).toBe(201);
-    expect(result.id).toBeDefined();
+    expect(status).toBe(200);
+    expect(result.limit).toBe(10);
+    expect(result.offset).toBe(0);
+    // console.log(result.items)
+    // expect(result.items[0].id).toBe(1);
+    // expect(result.items[0].title).toBe('Vegan Peanut Steak');
+    // expect(result.items[0].price).toBe(5390);
+    expect(result.items[0].category).toBe(1);
+  });
+  test('POST /menu/ add item to menu requires body', async () => {
+    // await insertData();
+    const id = 1
+    const { status } = await postAndParse(`/menu/${id}`, null);
+
+    expect(status).toBe(400);
   });
 });
