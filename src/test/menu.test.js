50ae0e1c8ad11cbe355f42903eb67cda241a6264
Ben-Ivars <60788806+Ben-Ivars@users.noreply.github.com>
2022-03-17 20:05:42 +0000

    menu test wow

--- src/test/menu.test.js
+++ src/test/menu.test.js
@@ -1,6 +1,9 @@
 import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
 import { createSchema, dropSchema, end, insertData } from '../lib/db';
-import { fetchAndParse, postAndParse } from './utils';
+import {
+  fetchAndParse, loginAsHardcodedAdminAndReturnToken, patchAndParse,
+  postAndParse
+} from './utils';
 
 describe('menus', () => {
   beforeAll(async () => {
@@ -26,10 +29,95 @@ describe('menus', () => {
     expect(result.items[0].category).toBe(1);
   });
   test('POST /menu/ add item to menu requires body', async () => {
-    // await insertData();
-    const id = 1
-    const { status } = await postAndParse(`/menu/${id}`, null);
+    const { status } = await postAndParse('/menu', null);
+    expect(status).toBe(401);
+  });
 
+  // test('POST /menu/ add item to menu requires body', async () => {
+  //   const body = {
+
+  //   }
+  //   const { status } = await postAndParse('/menu', body);
+  //   expect(status).toBe(200);
+  // });
+
+  test('GET /menu/ get items in category 1', async () => {
+    const { result, status } = await fetchAndParse('/menu?category=1', null);
+    expect(status).toBe(200);
+    expect(result.items[0].category).toBe(1);
+  });
+
+  test('GET /menu/ get items invalid category', async () => {
+    const { result, status } = await fetchAndParse('/menu?category=0', null);
+    expect(status).toBe(200);
+    expect(result.items.length).toBe(0);
+  });
+
+  test('GET /menu/ get items containing search query', async () => {
+    const { result, status } = await fetchAndParse('/menu?search=EkkiTil', null);
+    expect(status).toBe(200);
+    expect(result.items.length).toBe(0);
+  });
+
+  test('GET /menu/ no items found with search', async () => {
+    const { result, status } = await fetchAndParse('/menu?search=Vegan', null);
+    expect(status).toBe(200);
+    expect(result.items[0].id).toBe(1);
+    expect(result.items[0].title).toBe('Vegan Peanut Steak');
+    expect(result.items[0].price).toBe(5390);
+    expect(result.items[0].category).toBe(1);
+  });
+
+  test('GET /menu/ both search and category', async () => {
+    const { result, status } = await fetchAndParse('/menu?category=1&search=Vegan', null);
+    expect(status).toBe(200);
+    expect(result.items[0].id).toBe(1);
+    expect(result.items[0].title).toBe('Vegan Peanut Steak');
+    expect(result.items[0].price).toBe(5390);
+    expect(result.items[0].category).toBe(1);
+  });
+
+  test('GET /menu/1 get item with id 1', async () => {
+    const { result, status } = await fetchAndParse('/menu/1', null);
+    expect(status).toBe(200);
+    expect(result.id).toBe(1);
+    expect(result.title).toBe('Vegan Peanut Steak');
+    expect(result.price).toBe(5390);
+    expect(result.category).toBe(1);
+  });
+
+  test('GET /menu/1 get item with invalid id', async () => {
+    const { status } = await fetchAndParse('/menu/0', null);
     expect(status).toBe(400);
   });
+
+  test('PATCH /menu/1 no body', async () => {
+    const { status } = await patchAndParse('/menu/1', null);
+    expect(status).toBe(401);
+  });
+
+  test('PATCH /menu/1 update title of item 1', async () => {
+    const body = { title: 'new title' };
+    const token = await loginAsHardcodedAdminAndReturnToken();
+    const { result, status } = await patchAndParse('/menu/1', body, token);
+    expect(status).toBe(200);
+    expect(result.title).toBe('new title');
+  });
+
+  test('PATCH /menu/1 update multiple values of item 1', async () => {
+    const body = {
+      title: 'test title',
+      price: 12345,
+      description: 'test',
+      category: 2
+    };
+    const token = await loginAsHardcodedAdminAndReturnToken();
+    const { result, status } = await patchAndParse('/menu/1', body, token);
+    expect(status).toBe(200);
+    expect(result.title).toBe('test title');
+    expect(result.price).toBe(12345);
+    expect(result.description).toBe('test');
+    expect(result.category).toBe(2);
+  });
+
 });
